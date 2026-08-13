decfd92aef9710b3e5c3aab7c01701fef3cc6cfb
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2026-08-12 22:47:30 -0300

    chore: launch orb server without console window

--- start-kraken-orb.cmd
+++ start-kraken-orb.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 set "ROOT=%~dp0"
-set "PYTHON=C:\Users\thade\AppData\Roaming\uv\python\cpython-3.11-windows-x86_64-none\python.exe"
-if not exist "%PYTHON%" set "PYTHON=python.exe"
+set "PYTHONW=C:\Users\thade\AppData\Roaming\uv\python\cpython-3.11-windows-x86_64-none\pythonw.exe"
+if not exist "%PYTHONW%" set "PYTHONW=pythonw.exe"
 cd /d "%ROOT%"
-"%PYTHON%" -m http.server 47821 --bind 127.0.0.1 --directory "%ROOT%"
+"%PYTHONW%" -m http.server 47821 --bind 127.0.0.1 --directory "%ROOT%"
 endlocal
